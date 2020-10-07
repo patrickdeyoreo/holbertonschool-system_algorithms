280daaa867178e9b3cf4993a7bbc13ec24b35c1a
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-10-07 01:25:11 -0700

    Adding task 3

--- 0x00-red_black_tree/3-array_to_rb_tree.c
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -1,11 +1,22 @@
 #include "rb_trees.h"
 
 /**
- * array_to_rb_tree -
+ * array_to_rb_tree - create a red-black tree from an array
  *
- * Return: 
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ *
+ * Return: If creation fails, return NULL.
+ * Otherwise, return a pointer to the root of the newly created red-black tree.
  */
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
 {
+	rb_tree_t *tree = NULL;
 
+	if (array)
+	{
+		while (size--)
+			rb_tree_insert(&tree, *array++);
+	}
+	return (tree);
 }
