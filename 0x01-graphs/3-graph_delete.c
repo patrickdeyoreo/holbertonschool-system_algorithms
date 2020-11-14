c911e074a6a935b324d65a257ff304abfc93741c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-13 22:42:07 -0800

    Adding task 3

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -1,11 +1,28 @@
 #include "graphs.h"
 
 /**
- * graph_delete -
+ * graph_delete - delete a graph
  *
- * Return: 
+ * @graph: pointer to the graph to delete
  */
 void graph_delete(graph_t *graph)
 {
+	edge_t *edge = NULL;
+	vertex_t *vertex = NULL;
 
+	if (graph)
+	{
+		while ((vertex = graph->vertices))
+		{
+			graph->vertices = vertex->next;
+			while ((edge = vertex->edges))
+			{
+				vertex->edges = edge->next;
+				free(edge);
+			}
+			free(vertex->content);
+			free(vertex);
+		}
+		free(graph);
+	}
 }
