9a7dae5674df20d3d5567f224d3085d92ab11078
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-16 14:05:48 -0800

    Refactored task 4

--- 0x01-graphs/4-depth_first_traverse.c
+++ 0x01-graphs/4-depth_first_traverse.c
@@ -11,7 +11,8 @@
  * @visited: pointer to a table of visited vertex indices
  * @depth: depth of the starting vertex
  *
- * Return: Upon failure, return 0. Otherwise, return the greatest vertex depth.
+ * Return: Upon failure, return 0.
+ * Otherwise, return the greatest vertex depth.
  */
 static size_t dft(
 	const vertex_t *vertex, void (*action)(const vertex_t *, size_t),
@@ -29,6 +30,10 @@ static size_t dft(
 	action(vertex, depth);
 	for (edge = vertex->edges; edge; edge = edge->next)
 	{
+		if (visited[edge->dest->index])
+		{
+			continue;
+		}
 		new_depth = dft(edge->dest, action, visited, depth + 1);
 		if (max_depth < new_depth)
 		{
@@ -45,7 +50,8 @@ static size_t dft(
  * @graph: pointer to the graph to traverse
  * @action: pointer to a function to apply to each vertex
  *
- * Return: Upon failure, return 0. Otherwise, return the greatest vertex depth.
+ * Return: Upon failure, return 0.
+ * Otherwise, return the greatest vertex depth.
  */
 size_t depth_first_traverse(
 	const graph_t *graph, void (*action)(const vertex_t *, size_t))
