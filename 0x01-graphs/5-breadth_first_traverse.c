52f77e86ccecc0ad99d5ef5ff2ba8401cfea85ea
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-16 14:00:38 -0800

    Testing breadth first traversal

--- 0x01-graphs/5-breadth_first_traverse.c
+++ 0x01-graphs/5-breadth_first_traverse.c
@@ -8,22 +8,27 @@
  *
  * @vertex: pointer to the starting vertex
  * @action: pointer to a function to apply to each vertex
- * @visited: pointer to a table of visited vertex indices
  * @queue: pointer to the front of a vertex queue
+ * @queued: pointer to a table of queued vertices
  * @depths: pointer to a table of vertex depths
  *
- * Return: Upon failure, return 0. Otherwise, return the greatest vertex depth.
+ * Return: Upon failure, return 0.
+ * Otherwise, return the greatest vertex depth.
  */
 static size_t bft(
 	const vertex_t *vertex, void (*action)(const vertex_t *, size_t),
-	bool *visited, const vertex_t **queue, size_t *depths)
+	const vertex_t **queue, bool *queued, size_t *depths)
 {
 	const vertex_t **queue_rear = queue;
 	const edge_t *edge = NULL;
 	size_t depth = 0;
 
+	if (queued[vertex->index])
+	{
+		return (0);
+	}
 	*queue_rear++ = vertex;
-	visited[vertex->index] = true;
+	queued[vertex->index] = true;
 	depths[vertex->index] = 0;
 	while (queue != queue_rear)
 	{
@@ -32,12 +37,12 @@ static size_t bft(
 		action(vertex, depth);
 		for (edge = vertex->edges; edge; edge = edge->next)
 		{
-			if (visited[edge->dest->index])
+			if (queued[edge->dest->index])
 			{
 				continue;
 			}
 			*queue_rear++ = edge->dest;
-			visited[edge->dest->index] = true;
+			queued[edge->dest->index] = true;
 			depths[edge->dest->index] = depth + 1;
 		}
 	}
@@ -51,13 +56,14 @@ static size_t bft(
  * @graph: pointer to the graph to traverse
  * @action: pointer to a function to apply to each vertex
  *
- * Return: Upon failure, return 0. Otherwise, return the greatest vertex depth.
+ * Return: Upon failure, return 0.
+ * Otherwise, return the greatest vertex depth.
  */
 size_t breadth_first_traverse(
 	const graph_t *graph, void (*action)(const vertex_t *, size_t))
 {
-	bool *visited = NULL;
 	const vertex_t **queue = NULL;
+	bool *queued = NULL;
 	size_t *depths = NULL;
 	size_t depth = 0;
 
@@ -65,27 +71,27 @@ size_t breadth_first_traverse(
 	{
 		return (0);
 	}
-	visited = calloc(graph->nb_vertices, sizeof(*visited));
-	if (!visited)
-	{
-		return (0);
-	}
 	queue = calloc(graph->nb_vertices, sizeof(*queue));
 	if (!queue)
 	{
-		free(visited);
+		return (0);
+	}
+	queued = calloc(graph->nb_vertices, sizeof(*queued));
+	if (!queued)
+	{
+		free(queue);
 		return (0);
 	}
 	depths = calloc(graph->nb_vertices, sizeof(*depths));
 	if (!depths)
 	{
-		free(visited);
 		free(queue);
+		free(queued);
 		return (0);
 	}
-	depth = bft(graph->vertices, action, visited, queue, depths);
-	free(visited);
+	depth = bft(graph->vertices, action, queue, queued, depths);
 	free(queue);
+	free(queued);
 	free(depths);
 	return (depth);
 }
