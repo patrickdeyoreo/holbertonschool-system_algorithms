e2c30dc6b1ce59fa96ba65f71c716a530005cef2
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-12-08 20:28:40 -0800

    Testing alternate huffman tree with alt heap insert

--- 0x02-huffman_coding/heap/heap_insert.c
+++ 0x02-huffman_coding/heap/heap_insert.c
@@ -1,85 +1,92 @@
-#include <stdlib.h>
-
 #include "heap.h"
 
 /**
- * _heap_insert_sift_up - sift data upward
- *
- * @heap: pointer to the heap in which data was inserted
- * @node: pointer to the inserted node
- *
- * Return: pointer to the node containing the inserted data
+ * get_nth_node - get the nth node where next node should be inserted
+ * @node: heap
+ * @n: index of node to be inserted
+ * Return: parent if n is +1 than size or nth
+ * node which will store the next inserted node
  */
-static binary_tree_node_t *_heap_insert_sift_up(
-	heap_t *heap, binary_tree_node_t *node)
+static binary_tree_node_t *get_nth_node(binary_tree_node_t *node, size_t n)
 {
-	binary_tree_node_t *parent = node->parent;
-	void *data = node->data;
+	int index = 0, mask;
 
-	while (parent && heap->data_cmp(parent->data, data) > 0)
+	for (index = 0; 1 << (index + 1) <= (int)n; ++index)
+		;
+	for (--index; index >= 0; --index)
 	{
-		node->data = parent->data;
-		parent->data = data;
-		node = parent;
-		parent = node->parent;
+		mask = 1 << index;
+		if (n & mask)
+		{
+			if (node->right)
+				node = node->right;
+			else
+				break;
+		}
+		else
+		{
+			if (node->left)
+				node = node->left;
+			else
+				break;
+		}
 	}
 	return (node);
 }
 
 /**
- * _heap_insert - insert a value into a binary heap
- *
- * @heap: pointer to the heap in which to insert data
- * @node: pointer to the node to insert
- *
- * Return: pointer to the node containing the inserted data
+ * heapify - heapifies node
+ * @heap: heap
+ * @node: inserted node
+ * Return: returns the current node
  */
-static binary_tree_node_t *_heap_insert(
-	heap_t *heap, binary_tree_node_t *node)
+static binary_tree_node_t *heapify(heap_t *heap, binary_tree_node_t *node)
 {
-	binary_tree_node_t *parent = heap->root;
-	size_t path = heap->size >> 1;
-	size_t msb = 0;
+	void *temp;
 
-	while ((path >> msb) > 1)
-		msb += 1;
-	while (msb--)
-		parent = (path >> msb) & 1 ? parent->right : parent->left;
-	if (heap->size & 1)
-		parent->right = node;
-	else
-		parent->left = node;
-	node->parent = parent;
-	return (_heap_insert_sift_up(heap, node));
+	if (!node || !node->parent)
+		return (node);
+	while (node->parent)
+	{
+		if (heap->data_cmp(node->parent->data, node->data) > 0)
+		{
+			temp = node->data;
+			node->data = node->parent->data;
+			node->parent->data = temp;
+		}
+		node = node->parent;
+	}
+	return (node);
 }
 
 /**
- * heap_insert - insert a value into a binary heap
- *
- * @heap: pointer to the heap in which to insert data
- * @data: pointer to the data to insert
- *
- * Return: If heap is NULL or memory allocation fails, return NULL.
- * Otherwise, returh a pointer to the node containing data.
+ * heap_insert - insert into min heap
+ * @heap: heap data structure
+ * @data: data for a new node
+ * Return: pointer to the created node containing data, or NULL if it fails
  */
 binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 {
-	binary_tree_node_t *node = NULL;
+	binary_tree_node_t *node, *parent;
 
 	if (!heap)
 		return (NULL);
 	node = binary_tree_node(NULL, data);
 	if (!node)
 		return (NULL);
-	if (!heap->size)
+	if (!heap->root)
 	{
-		heap->size = 1;
 		heap->root = node;
 	}
 	else
 	{
-		heap->size += 1;
-		node = _heap_insert(heap, node);
+		parent = get_nth_node(heap->root, heap->size + 1);
+		if (!parent->left)
+			parent->left = node;
+		else
+			parent->right = node;
+		node->parent = parent;
 	}
-	return (node);
+	heap->size += 1;
+	return (heapify(heap, node));
 }
