0a4c7fe66abfd988ccb989a3951ea979d54513f2
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-12-08 20:41:58 -0800

    Testing huffman tree with old extract

--- 0x02-huffman_coding/heap/heap_extract.c
+++ 0x02-huffman_coding/heap/heap_extract.c
@@ -3,109 +3,90 @@
 #include "heap.h"
 
 /**
- * get_nth_node - get the nth node where next node should be inserted
- * @node: heap
- * @n: index of node to be inserted
- * Return: parent if n is +1 than size or nth
- * node which will store the next inserted node
+ * _heap_extract_sift_down - sift data downward
+ *
+ * @heap: pointer to the heap from which data was extracted
  */
-static binary_tree_node_t *get_nth_node(binary_tree_node_t *node, size_t n)
+static void _heap_extract_sift_down(heap_t *heap)
 {
-	int index = 0, mask;
+	binary_tree_node_t *root = heap->root;
+	binary_tree_node_t *swap = root;
+	binary_tree_node_t *lchild = NULL;
+	binary_tree_node_t *rchild = NULL;
+	void *data = root->data;
 
-	for (index = 0; 1 << (index + 1) <= (int)n; ++index)
-		;
-	for (--index; index >= 0; --index)
+	while (root)
 	{
-		mask = 1 << index;
-		if (n & mask)
-		{
-			if (node->right)
-				node = node->right;
-			else
-				break;
-		}
-		else
-		{
-			if (node->left)
-				node = node->left;
-			else
-				break;
-		}
-	}
-	return (node);
-}
-
-/**
- * swap - swap data of two nodes
- * @n1: node1
- * @n2: node2
- */
-static void swap(binary_tree_node_t *n1, binary_tree_node_t *n2)
-{
-	void *temp;
-
-	temp = n1->data;
-	n1->data = n2->data;
-	n2->data = temp;
-}
-
-/**
- * sift_down - heapifies node
- * @heap: heap
- */
-static void sift_down(heap_t *heap)
-{
-	binary_tree_node_t *largest, *node;
-
-	if (!heap || !heap->root || heap->size < 2)
-		return;
-	node = heap->root;
-	while (node->left)
-	{
-		largest = node->left;
-		if (node->right && heap->data_cmp(node->data, node->right->data) >= 0
-		    && heap->data_cmp(node->right->data, node->left->data) < 0)
-		{
-			swap(node->right, node);
-			largest = node->right;
-		}
-		else if (heap->data_cmp(node->left->data, node->data) <= 0)
-		{
-			swap(node->left, node);
-		}
-		node = largest;
+		lchild = root->left;
+		rchild = root->right;
+		if (lchild && heap->data_cmp(swap->data, lchild->data) > 0)
+			swap = lchild;
+		if (rchild && heap->data_cmp(swap->data, rchild->data) > 0)
+			swap = rchild;
+		if (swap == root)
+			return;
+		root->data = swap->data;
+		swap->data = data;
+		root = swap;
 	}
 }
 
 /**
- * heap_extract - extracts root node from heap (min value)
- * @heap: heap
- * Return: data of root node
+ * _heap_extract - extract the value at the root of a binary heap
+ *
+ * @heap: pointer to the heap from which to extract data
  */
-void *heap_extract(heap_t *heap)
+static void _heap_extract(heap_t *heap)
 {
-	binary_tree_node_t *last_node;
-	void *data;
+	binary_tree_node_t *parent = heap->root;
+	size_t path = heap->size >> 1;
+	size_t msb = 0;
 
-	if (!heap || !heap->root || heap->size <= 0)
-		return (NULL);
-	data = heap->root->data;
-	last_node = get_nth_node(heap->root, heap->size);
-	heap->root->data = last_node->data;
-	if (last_node->parent)
+	while ((path >> msb) > 1)
+		msb += 1;
+	while (msb--)
+		parent = (path >> msb) & 1 ? parent->right : parent->left;
+	if (heap->size & 1)
 	{
-		if (last_node->parent->left == last_node)
-			last_node->parent->left = NULL;
-		else
-			last_node->parent->right = NULL;
+		heap->root->data = parent->right->data;
+		free(parent->right);
+		parent->right = NULL;
 	}
 	else
 	{
-		heap->root = NULL;
+		heap->root->data = parent->left->data;
+		free(parent->left);
+		parent->left = NULL;
 	}
-	free(last_node);
-	heap->size -= 1;
-	sift_down(heap);
+	_heap_extract_sift_down(heap);
+}
+
+
+/**
+ * heap_extract - extract the value at the root of a binary heap
+ *
+ * @heap: pointer to the heap from which to extract data
+ *
+ * Return: If heap is NULL or empty, return NULL.
+ * Otherwise, returh a pointer the extracted data.
+ */
+void *heap_extract(heap_t *heap)
+{
+	void *data = NULL;
+
+	if (!heap || !heap->size || !heap->root)
+		return (NULL);
+	data = heap->root->data;
+	if (heap->size == 1)
+	{
+		free(heap->root);
+		heap->root = NULL;
+		heap->size = 0;
+	}
+	else
+	{
+		_heap_extract(heap);
+		heap->size -= 1;
+	}
 	return (data);
 }
